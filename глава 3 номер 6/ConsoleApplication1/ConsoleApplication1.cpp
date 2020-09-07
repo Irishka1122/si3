012188bc5d7a5d79bf17e2634f0df07201555284
popcornhub <nikolay173@yandex.ru>
2020-09-07 14:46:50 +0500

    номер 6 главы 3

---
+++ глава 3 номер 6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e;
+	cin >> a >> b >> c;
+	if (a > b) {
+		d = max(a, c);
+		e = min(b, c);
+	}
+	else
+	{
+		d = max(b, c);
+		e = min(a, c);
+	}
+	cout << e << ',' << (a + b + c - d - e) << ',' << d;
+}
+

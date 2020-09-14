26c0be9382e846de1d13b83da01748f36b1abd9d
popcornhub <nikolay173@yandex.ru>
2020-09-14 16:14:13 +0500

    new

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include "std_lib_facilities.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	vector <double> ch;
+	int N;
+	double sum = 0;
+	string c;
+	cin >> c;
+	while (c!= "|") {
+		ch.push_back(atof(c.c_str()));
+		cin >> c;
+	}
+	cout << "Введите количество чисел, которые хотите проссумировать"<<endl;
+	cin >> N;
+	try {
+		if (ch.size() >= N) {
+			for (int i = 0; i <= N - 1; i++) {
+				cout << ch[i] << ' ';
+				sum += ch[i];
+			}
+			cout << "Сумма первых " << N << " чисел равна " << sum;
+		}
+		else error("Недостаточно чисел.");
+	}
+	catch (runtime_error) {
+		cout << "1";
+	};
+}
+
---
+++ глава 5 номер 7/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class bad_arguments {};
+
+vector <double> roots(int a,int b,int c) {
+	vector <double> m;
+	int d = b * b - 4 * a * c;
+	if (d > 0) {
+		m.push_back((-b - sqrt(d)) / (2 * a));
+		m.push_back((-b + sqrt(d)) / (2 * a));
+		return m;
+	}
+	if (d == 0) {
+		m.push_back((-b ) / (2 * a));
+		return m;
+	}
+	if (d < 0) {
+		throw bad_arguments();
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	vector <double> res;
+	cin >> a >> b >> c;
+	try {
+		res = roots(a, b, c);
+		if (res.size() == 1)
+			cout << res[0];
+		else
+			cout << res[0] << " " << res[1];
+	}
+	catch (bad_arguments) {
+		cout << "Уравнение не имеет действительных корней.";
+		return 1;
+	}
+}
+
+
